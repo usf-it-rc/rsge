0640b41068156b4327ac8bc89531dd01e73cf53e
Brian Smith <brs@usf.edu>
2012-12-12 15:48:54 -0500

    Watch your typos

--- tests/rsgequeue_spec.rb
+++ tests/rsgequeue_spec.rb
@@ -4,14 +4,15 @@ require 'rsgequeue'
 
 $rspec_init = true
 
-describe RsgeQueue
+describe RsgeQueue do
     before :all do
         @queues = RsgeQueue.new
     end
 
     it "returns a list of queues" do
-        @queues.list.should eql(["default","xlong"])
-    done
+        @queues.list.should eql([:default, :xlong])
+    end
+end
 
 #queues.each do |queue|
 #    puts queue.name.to_s + " " + queue.used.to_s + " " + queue.avail.to_s + " " + queue.total.to_s
